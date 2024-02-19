9bfa794af50c3b3d0e3b7daaa13fb1a900deeb66
FelineJTD <feliciasutandijo@gmail.com>
2024-02-19 23:19:27 +0700

    chore: delete unused console.log

--- public/js/index.js
+++ public/js/index.js
@@ -10,16 +10,10 @@ function encrypt() {
     method: 'POST',
     data: { plaintext: plaintext, cipher: cipher, key: key },
     success: function(response) {
-      console.log("success");
-      console.log(response);
-      // Handle the response from the server
-      // display response in a div with id "response"
       $('#response').html(response);
     },
     error: function(error) {
-      console.error("error");
       console.error(error);
-      // Handle errors
     }
   });
 }
