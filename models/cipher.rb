98bc1b4a38de01ae90bfc14b06fd689b6cc3c059
FelineJTD <13520050@std.stei.itb.ac.id>
2024-02-20 03:21:48 +0100

    fix: a=1 to a=0

--- models/cipher.rb
+++ models/cipher.rb
@@ -81,7 +81,7 @@ class Cipher
     self.sanitize
     @ciphertext = ''
     for i in 0..@plaintext.length-1
-      @ciphertext += (((@plaintext[i].ord - 65) + (@key[i % @key.length].ord - 64)) % 26 + 65).chr
+      @ciphertext += (((@plaintext[i].ord - 65) + (@key[i % @key.length].ord - 65)) % 26 + 65).chr
     end
   end
 
@@ -89,7 +89,9 @@ class Cipher
     self.sanitize
     @plaintext = ''
     for i in 0..@ciphertext.length-1
-      @plaintext += ((((@ciphertext[i].ord - 65) - (@key[i % @key.length].ord - 64)) + 26) % 26 + 65).chr
+      @plaintext += ((((@ciphertext[i].ord - 65) - (@key[i % @key.length].ord - 65)) + 26) % 26 + 65).chr
     end
   end
+
+
 end
