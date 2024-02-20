f4c6240b0b5f5d6dc071c6816fae39471fada27e
FelineJTD <feliciasutandijo@gmail.com>
2024-02-20 23:02:18 +0700

    feat: handle txt file input

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,34 @@
 // public/js/index.js
 
+// ENCRYPT
 function encrypt() {
   console.log('Encrypting plaintext');
+
+  type = $('#input-type').val();
   plaintext = $('#input-text').val();
   cipher = $('#cipher').val();
   key = $('#input-key').val();
+
+  // read file
+  if (type == 'file') {
+    var file = document.getElementById('input-file').files[0];
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      plaintext = e.target.result;
+      callEncrypt({ type: type, plaintext: plaintext, cipher: cipher, key: key });
+    }
+    reader.readAsText(file);
+  }
+  else {
+    callEncrypt({ type: type, plaintext: plaintext, cipher: cipher, key: key });
+  }
+}
+
+function callEncrypt(data) {
   $.ajax({
     url: '/encrypt',
     method: 'POST',
-    data: { plaintext: plaintext, cipher: cipher, key: key },
+    data: data,
     success: function(response) {
       $('#response').html(response);
     },
@@ -18,15 +38,35 @@ function encrypt() {
   });
 }
 
+
+// DECRYPT
 function decrypt() {
   console.log('Decrypting ciphertext');
+  
+  type = $('#input-type').val();
   ciphertext = $('#input-text').val();
   cipher = $('#cipher').val();
   key = $('#input-key').val();
+
+  if (type == 'file') {
+    var file = document.getElementById('input-file').files[0];
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      ciphertext = e.target.result;
+      callDecrypt({ type: type, ciphertext: ciphertext, cipher: cipher, key: key });
+    }
+    reader.readAsText(file);
+  }
+  else {
+    callDecrypt({ type: type, ciphertext: ciphertext, cipher: cipher, key: key });
+  }
+}
+
+function callDecrypt(data) {
   $.ajax({
     url: '/decrypt',
     method: 'POST',
-    data: { ciphertext: ciphertext, cipher: cipher, key: key },
+    data: data,
     success: function(response) {
       $('#response').html(response);
     },
